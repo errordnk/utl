3086bbd5c0e1051066612cda2e68e8bfb7a6465a
Dmitriy Krasnov <dnk@sarnet.ru>
2025-08-23 12:35:46 +0400

    Update utl.go

--- utl.go
+++ utl.go
@@ -6,9 +6,28 @@ import (
     "math"
 )
 
-func Rndstr(l int) string {
+func RndStr(l int) string {
     buff := make([]byte, int(math.Ceil(float64(l)/float64(1.33333333333))))
     rand.Read(buff)
     str := base64.RawURLEncoding.EncodeToString(buff)
-    return str[:l] // strip 1 extra character we get from odd length results
+    return str[:l]
+}
+
+func Rows(path string) ([]string, error) {
+    var client http.Client
+    resp, err := client.Get("https://sarnet.ru/" + path)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+    var body string
+    if resp.StatusCode == http.StatusOK {
+        bodyBytes, err := io.ReadAll(resp.Body)
+        if err != nil {
+            return nil, err
+        }
+        body = string(bodyBytes)
+    }
+    rows := strings.Split(body, "\n")
+    return rows, nil
 }
